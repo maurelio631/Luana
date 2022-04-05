fbb54f738458f6f7e07b7db68998f7a85fd91f8e
maurelio631 <marco.silva631@outlook.com>
2022-04-05 20:58:26 -0300

    função de delay melhorada armazenando cache para cancelar o delay caso a página já tenha sido visitada

--- script/script.js
+++ script/script.js
@@ -1,15 +1,24 @@
 const conteudoEscondido = document.getElementById("wrapContent");
 const botaoPlayYoutube = document.getElementsByClassName("ytp-large-play-button")
 const botao = [...botaoPlayYoutube]
-
+const cookies = localStorage.getItem('namespace.visited');
 function comprar() {
     window.open("https://pay.kiwify.com.br/DFdM6CV", "_blank")
 }
 
 function delay() {
-    setTimeout(() => {
+    if (cookies == null) {
+        setTimeout(() => {
+            conteudoEscondido.style.display = "flex"
+        }, 1200000)
+        localStorage.setItem('namespace.visited', 1)
+        console.log(cookies)
+    }else{
         conteudoEscondido.style.display = "flex"
-    },10000)
+        localStorage.setItem('namespace.visited', 1)
+        console.log(cookies)
+    }
 }
 
 delay();
+
