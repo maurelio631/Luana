153b44c89455cfd0d4c13483b0936806fd5f3974
maurelio631 <marco.silva631@outlook.com>
2022-05-29 22:26:29 -0300

    Correção delay

--- script/script.js
+++ script/script.js
@@ -1,24 +1,22 @@
 const conteudoEscondido = document.getElementById("wrapContent");
-const botaoPlayYoutube = document.getElementsByClassName("ytp-large-play-button")
-const botao = [...botaoPlayYoutube]
-const cookies = localStorage.getItem('namespace.visited');
+
 function comprar() {
-    window.open("https://pay.kiwify.com.br/DFdM6CV", "_blank")
+  window.open("https://pay.kiwify.com.br/DFdM6CV", "_blank");
 }
 
 function delay() {
-    if (cookies == null) {
-        setTimeout(() => {
-            conteudoEscondido.style.display = "flex"
-        }, 1200000)
-        localStorage.setItem('namespace.visited', 1)
-        console.log(cookies)
-    }else{
-        conteudoEscondido.style.display = "flex"
-        localStorage.setItem('namespace.visited', 1)
-        console.log(cookies)
-    }
+  setTimeout(() => {
+    conteudoEscondido.style.display = "flex";
+  }, 1200000);
 }
 
-delay();
+function checkLocalStorage() {
+  if (localStorage == null) {
+    localStorage.setItem("visited", "yes");
+    delay();
+  } else {
+    conteudoEscondido.style.display = "flex";
+  }
+}
 
+checkLocalStorage();
